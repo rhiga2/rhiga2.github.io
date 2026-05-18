87f0cc9a1993fa231aec3bd25c62d46e74bc8833
Ryley Higa <rhiga@sumologic.com>
2026-05-17 16:44:08 -1000

    Added Astro Islands greeting message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   site: 'https://rhiga2.github.io',
+  integrations: [preact()],
 })
