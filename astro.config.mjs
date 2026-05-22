1b98f183fdf06195cae4fc427f39683ff2583055
Ryley Higa <rhiga@sumologic.com>
2026-05-22 11:35:39 -1000

    feat: add mdx integration and tailwind vite plugin to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config'
-
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://rhiga2.github.io',
-  integrations: [preact()],
-})
+  integrations: [preact(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
